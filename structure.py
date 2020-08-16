ea29be594c0206daa3484815053aa48ac8005cf4
Nathan Turner <52763514+NathanTurner270@users.noreply.github.com>
2020-08-16 10:30:04 -0600

    Added skeleton for paradboxes module setup

---
+++ structure.py
@@ -0,0 +1,14 @@
+class Cube:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def is_main_cube(self):
+        pass
+
+    def change_color(self):
+        pass
