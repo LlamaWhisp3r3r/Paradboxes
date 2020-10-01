c7ec727d161d3342d27397515e347fdb8043e5e8
ntswizzle <nathanturner270@gmail.com>
2020-10-01 14:23:20 -0600

    Testing 80

--- paradboxes/setup.py
+++ paradboxes/setup.py
@@ -58,9 +58,9 @@ class InitializeBoard:
         self.red_pwm = PWMLED(self.led_pins[0])
         self.green_pwm = PWMLED(self.led_pins[1])
         self.blue_pwm = PWMLED(self.led_pins[2])
-        self.red_pwm.frequency = 500
-        self.green_pwm.frequency = 500
-        self.blue_pwm.frequency = 500
+        self.red_pwm.frequency = 80
+        self.green_pwm.frequency = 80
+        self.blue_pwm.frequency = 80
         logging.info("LED Pins initialized")
 
 
